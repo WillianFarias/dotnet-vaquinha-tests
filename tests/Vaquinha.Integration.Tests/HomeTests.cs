d412219f000b2ddf00b7826c0d5244189c9dea76
Will <williansousafarias@gmail.com>
2021-01-21 22:50:36 -0300

    testes implementados, testando porcentagem de 20%

---
+++ tests/Vaquinha.Integration.Tests/HomeTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using System.Threading.Tasks;
+using Vaquinha.Domain.Extensions;
+using Vaquinha.Integration.Tests.Fixtures;
+using Vaquinha.MVC;
+using Xunit;
+
+namespace Vaquinha.Integration.Tests
+{
+    [Collection(nameof(IntegrationWebTestsFixtureCollection))]
+    public class HomeTests
+    {
+        private readonly IntegrationTestsFixture<StartupWebTests> _integrationTestsFixture;
+
+        public HomeTests(IntegrationTestsFixture<StartupWebTests> integrationTestsFixture)
+        {
+            _integrationTestsFixture = integrationTestsFixture;
+        }
+
+        [Trait("HomeControllerIntegrationTests", "HomeController_CarregarPaginaInicial_TotalDoadoresETotalValorArrecadadoDeveSerZero")]
+        [Fact]
+        public async Task HomeController_CarregarPaginaInicial_TotalDoadoresETotalValorArrecadadoDeveSerZero()
+        {
+            // Arrange & Act
+            var home = await _integrationTestsFixture.Client.GetAsync("Home");
+
+            // Assert
+            home.EnsureSuccessStatusCode();//verificando se o retorno é 200
+            var dadosHome = await home.Content.ReadAsStringAsync();//retorna todo o html
+
+            var totalArrecadado = 0.ToDinheiroBrString();
+            var metaCampanha = _integrationTestsFixture.ConfiguracaoGeralAplicacao.MetaCampanha.ToDinheiroBrString();
+
+            // Dados totais da doação
+            dadosHome.Should().Contain(expected: "Arrecadamos quanto?");
+            dadosHome.Should().Contain(expected: totalArrecadado);//espera 0 reais
+
+            dadosHome.Should().Contain(expected: "Quanto falta arrecadar?");
+            dadosHome.Should().Contain(expected: metaCampanha);//espera 50000
+        }
+    }
+}
